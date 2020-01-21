7fe1a44b57ea4412722914dd61d8cb7843be71e1
Horst Possegger <possegger@icg.tugraz.at>
2020-01-21 15:25:00 +0100

    gnome-terminal startup

---
+++ environment/default-shell.sh
@@ -0,0 +1,4 @@
+#!/bin/bash --
+# TODO check https://superuser.com/a/198022 and document!
+BASH_POST_RC="cd ~/workspace; source .venv3/bin/activate; (echo \"Sourced the default python3 venv\"; echo \"Enjoy\") | cowsay -W80 -n" gnome-terminal --geometry=80x50+3000+0
+
